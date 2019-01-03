78f42d74b12df28423b8f1ff91ba41f0a8d4f349
DESKTOP-38OQRU0\Amol <amoljp19@gmail.com>
2019-01-03 15:30:16 +0530

    added CityWeatherApplication class

---
+++ app/src/main/java/com/softaai/cityweather/CityWeatherApplication.kt
@@ -0,0 +1,14 @@
+package com.softaai.cityweather
+
+import android.app.Application
+
+
+/**
+ * Created by Amol Pawar on 03-01-2019.
+ * softAai Apps
+ */
+
+class CityWeatherApplication : Application()
+{
+    
+}
--- app/src/main/java/com/softaai/cityweather/data/Temp.kt
+++
@@ -1,7 +0,0 @@
-package com.softaai.cityweather.data
-
-
-/**
- * Created by Amol Pawar on 03-01-2019.
- * softAai Apps
- */
