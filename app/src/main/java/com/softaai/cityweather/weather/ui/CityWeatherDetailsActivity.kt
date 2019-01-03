b68c50ec85ca2b3704b7943f4b1fb5de3ec65454
DESKTOP-38OQRU0\Amol <amoljp19@gmail.com>
2019-01-03 17:37:20 +0530

    added reactive network and rx binding library and related functionality code

---
+++ app/src/main/java/com/softaai/cityweather/weather/ui/CityWeatherDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.softaai.cityweather.weather.ui
+
+import android.support.v7.app.AppCompatActivity
+
+
+/**
+ * Created by Amol Pawar on 03-01-2019.
+ * softAai Apps
+ */
+
+class CityWeatherDetailsActivity : AppCompatActivity()
+{
+
+}
