8d0cef5ac877518b25e6b19cb362e7c64ad13099
DESKTOP-38OQRU0\Amol <amoljp19@gmail.com>
2019-01-03 17:18:07 +0530

    added CityWeatherSearchActivity and its layout file also resolved Error inflating class android.support.design.widget.TextInputLayout

---
+++ app/src/main/java/com/softaai/cityweather/weather/ui/CityWeatherSearchActivity.kt
@@ -0,0 +1,20 @@
+package com.softaai.cityweather.weather.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.softaai.cityweather.R
+
+
+/**
+ * Created by Amol Pawar on 03-01-2019.
+ * softAai Apps
+ */
+
+class CityWeatherSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_weather_search)
+    }
+
+}
